1287bb43603b454ce8e265701608d8a05e2ae403
Jaroslav Havrda <jarda666jarda@gmail.com>
2024-01-20 09:57:59 +0100

    Splitting two files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "include/SDL_wrappers.h"
+#include "include/ImGUI_Wrapper.h"
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_opengl3.h"
@@ -8,99 +9,6 @@
 #include <sstream>
 #include <exception>
 
-class SDL_GLContext_Holder
-{
-    public:
-    SDL_GLContext gl_context;
-    SDL_GLContext_Holder( SDL_Window* window)
-    {
-        gl_context = SDL_GL_CreateContext(window);
-    }
-    ~SDL_GLContext_Holder()
-    {
-        SDL_GL_DeleteContext(gl_context);
-    }
-};
-
-class Imgui_Context_Holder
-{
-    public:
-    Imgui_Context_Holder()
-    {
-        ImGui::CreateContext();
-    }
-    ~Imgui_Context_Holder()
-    {
-        ImGui::DestroyContext();
-    }
-};
-
-void set_sdl_attributes()
-{
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-
-    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
-
-    // Create window with graphics context
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-}
-
-class window_holder
-{
-    SDL_Window *window;
-
-public:
-    window_holder()
-    {
-        SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-        window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-        if (!window)
-        {
-            std::stringstream ss;
-            ss << "Error: SDL_CreateWindow() " << SDL_GetError();
-            throw std::runtime_error(ss.str());
-        }
-    }
-    ~window_holder()
-    {
-        SDL_DestroyWindow(window);
-    }
-    auto sdl_window()
-    {
-        return window;
-    }
-};
-
-class imgui_impl_opengl3
-{
-    public:
-    imgui_impl_opengl3(SDL_Window * window, SDL_GLContext & gl_context)
-    {
-        ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-    }
-    ~imgui_impl_opengl3()
-    {
-        ImGui_ImplOpenGL3_Shutdown();
-    }
-};
-
-class imgui_impl_sdl2{
-    public:
-    imgui_impl_sdl2()
-    {
-        const char* glsl_version = "#version 130";
-        ImGui_ImplOpenGL3_Init(glsl_version);
-    }
-    ~imgui_impl_sdl2()
-    {
-        ImGui_ImplSDL2_Shutdown();
-    }
-};
 
 bool should_quit( SDL_Window * window)
 {
